01209c8b88ba139ef2af284dc42eb724f3bb0270
ReynaldyNC <reynaldy.nur.cahyo.003@gmail.com>
2023-03-25 17:41:28 +0700

    Added Insertion Sort

--- Main.cpp
+++ Main.cpp
@@ -35,6 +35,20 @@ std::vector<int> selectionSort(std::vector<int> number) {
 	return number;
 }
 
+std::vector<int> insertionSort(std::vector<int> number) {
+	for (int i = 1; i <= number.size() - 1; i++) {
+		for (int j = i; j > 0; j--) {
+			if (number[j - 1] > number[j]) {
+				int temp = number[j - 1];
+				number[j - 1] = number[j];
+				number[j] = temp;
+			}
+		}
+	}
+
+	return number;
+}
+
 int main() {
 	std::vector<int> number = { 8, 9, 2, 1, 5, 3, 7, 0, 4, 6};
 
@@ -46,11 +60,16 @@ int main() {
 	std::cout << std::endl;
 
 	std::cout << "Sorted: ";
+	// Uncomment sorting algorithm you want to use
 	/*for (int n : bubbleSort(number)) {
 		std::cout << n;
 	}*/
 	
-	for (int n : selectionSort(number)) {
+	/*for (int n : selectionSort(number)) {
+		std::cout << n;
+	}*/
+
+	for (int n : insertionSort(number)) {
 		std::cout << n;
 	}
 
